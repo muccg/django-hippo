9b5d5d66ec7d8bc3d1b0ef997469372aedd90269
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2015-03-03 15:24:12 +0800

    fix path detection for contrib assets

--- hippo/hp/management/commands/load_hp.py
+++ hippo/hp/management/commands/load_hp.py
@@ -2,6 +2,8 @@ from pkg_resources import resource_filename
 from optparse import make_option
 from django.core.management.base import BaseCommand
 from ...ontology import load_hp_owl, Phenotype
+import hippo
+import os
 
 
 class Command(BaseCommand):
@@ -20,5 +22,5 @@ class Command(BaseCommand):
             self.stdout.write("Removing %d objects" % Phenotype.objects.count())
             Phenotype.objects.all().delete()
 
-        filename = resource_filename('hippo', 'contrib/HPO/hp.owl')
+        filename = os.path.join(hippo.__path__[0], resource_filename('hippo', 'contrib/HPO/hp.owl'))
         load_hp_owl(filename)
--- hippo/hp/management/commands/load_phenotips.py
+++ hippo/hp/management/commands/load_phenotips.py
@@ -4,11 +4,13 @@ from pkg_resources import resource_filename
 from optparse import make_option
 from django.core.management.base import BaseCommand
 from ...ontology import load_phenotips_categories
+import hippo
+import os
 
 
 class Command(BaseCommand):
     help = 'Imports the HP in OWL format'
 
     def handle(self, *args, **options):
-        filename = resource_filename('hippo', 'contrib/PhenoTips/Generic phenotype configuration.xml')
+        filename = os.path.join(hippo.__path__[0], resource_filename('hippo', 'contrib/PhenoTips/Generic phenotype configuration.xml'))
         load_phenotips_categories(filename)
